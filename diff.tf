f88963c5798f6559786ada2c58de90a6ba26a21d
venkateshtammali <39961342+venkateshtammali@users.noreply.github.com>
2019-10-21 16:42:28 +0530

    Update diff.tf

--- diff.tf
+++ diff.tf
@@ -8,6 +8,10 @@ variable "name" {
     type = "String"
 }
 
+variable "name" {
+    type = "String"
+}
+
 provider "aws" {
   access_key = "${var.AWS_ACCESS_KEY}"
   secret_key = "${var.AWS_SECRET_KEY}"
